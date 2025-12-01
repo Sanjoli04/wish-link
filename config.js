0669289fe018f200405deea2e4e4b6ce330937a8
Sanjoli04 <vashisthsanjoli@gmail.com>
2025-12-01 21:28:47 +0530

    Initial Commit

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+const CONFIG = {
+  SUPABASE_URL: "https://bcyhqarsqnvdtudhrefp.supabase.co",
+  SUPABASE_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjeWhxYXJzcW52ZHR1ZGhyZWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1NTM1MDQsImV4cCI6MjA4MDEyOTUwNH0.LpU324o2Xu9Mz7AYej6wQvgQZll26Qm-_CdSQeqUiIs",
+  CLOUDINARY_CLOUD_NAME: "drpqted9t",
+  CLOUDINARY_PRESET: "wishlink_uploads",
+  RAZORPAY_KEY: "rzp_test_RmCMRMVC1NXKhS"
+};
